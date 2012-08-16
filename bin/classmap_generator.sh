d410fd1a5bd70b7fa6cc57166d19d2326f8c60fd
Jérémy Huet <jeremy.huet@gmail.com>
2012-08-16 17:26:27 +0200

    Adding a binary to create classmap file for Dibber module. Execute it from the project root.

---
+++ bin/classmap_generator.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/usr/local/bin/php54 vendor/zendframework/zendframework/bin/classmap_generator.php -l module/Dibber/
--- init_autoloader.php
+++ init_autoloader.php
@@ -8,8 +8,8 @@
  */
 
 // Composer autoloading
-$loader = require 'vendor/autoload.php';
+require 'vendor/autoload.php';
 
 if (!class_exists('Zend\Loader\AutoloaderFactory')) {
-    throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.');
+    throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install`.');
 }
