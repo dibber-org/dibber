611f3a8e18a7243b1fee90fdb928db66a7256d15
Jérémy Huet <jeremy.huet@gmail.com>
2012-10-26 16:10:55 +0200

    array() is so PHP < 5.4 but hard to forget :) [ci skip]

--- module/Dibber/src/Dibber/Controller/HomeController.php
+++ module/Dibber/src/Dibber/Controller/HomeController.php
@@ -27,7 +27,7 @@ class HomeController extends AbstractActionController
         $userMapper = $this->getServiceLocator()->get('dibber_user_mapper');
         $user = $userMapper->findByLogin($login);
         if ($user instanceof Document\User) {
-            return $this->forward()->dispatch('Dibber\Controller\User', array('action' => 'index', 'user' => $user));
+            return $this->forward()->dispatch('Dibber\Controller\User', ['action' => 'index', 'user' => $user]);
         }
 
         # Check if it is a Place
@@ -35,7 +35,7 @@ class HomeController extends AbstractActionController
         $placeMapper = $this->getServiceLocator()->get('dibber_place_mapper');
         $place = $placeMapper->findByLogin($login);
         if ($place instanceof Document\Place) {
-            return $this->forward()->dispatch('Dibber\Controller\Place', array('action' => 'index', 'place' => $place));
+            return $this->forward()->dispatch('Dibber\Controller\Place', ['action' => 'index', 'place' => $place]);
         }
 
         # None found, generate 404 HTTP error
