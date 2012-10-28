d1b7de2ff80bcc3bbbe4434858cd061031c8b2da
Jérémy Huet <jeremy.huet@gmail.com>
2012-10-28 16:29:47 +0100

    Adding document serializer for toArray conversion.

---
+++ module/Dibber/src/Dibber/Document/Serializer.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * @see         https://github.com/borisguery/bgylibrary/blob/master/library/Bgy/Doctrine/EntitySerializer.php
+ * @see         https://gist.github.com/1034079#file_serializable_entity.php
+ */
+
+namespace Dibber\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\ClassMetadata,
+    Doctrine\Common\Util\Inflector,
+    Doctrine\ODM\MongoDB\DocumentManager,
+    Exception;
+
+class Serializer
+{
+    /**
+     * @var Doctrine\ODM\MongoDB\DocumentManager
+     */
+    protected $dm;
+
+    /**
+     * @var int
+     */
+    protected $recursionDepth = 0;
+
+    /**
+     * @var int
+     */
+    protected $maxRecursionDepth = 1;
+
+    public function __construct($dm)
+    {
+        $this->setDocumentManager($dm);
+    }
+
+    /**
+     * @return DocumentManager
+     */
+    public function getDocumentManager()
+    {
+        return $this->dm;
+    }
+
+    /**
+     * @param DocumentManager $dm
+     * @return Serializer
+     */
+    public function setDocumentManager(DocumentManager $dm)
+    {
+        $this->dm = $dm;
+
+        return $this;
+    }
+
+    /**
+     * Get the maximum recursion depth
+     *
+     * @return  int
+     */
+    public function getMaxRecursionDepth()
+    {
+        return $this->maxRecursionDepth;
+    }
+
+    /**
+     * Set the maximum recursion depth
+     *
+     * @param   int     $maxRecursionDepth
+     * @return  void
+     */
+    public function setMaxRecursionDepth($maxRecursionDepth)
+    {
+        $this->maxRecursionDepth = $maxRecursionDepth;
+    }
+
+    protected function serialize($document)
+    {
+        $className = get_class($document);
+        $metadata = $this->dm->getClassMetadata($className);
+
+        $data = array();
+
+        foreach ($metadata->fieldMappings as $field => $mapping) {
+            $value = $metadata->reflFields[$field]->getValue($document);
+            $field = Inflector::tableize($field);
+            if ($value instanceof \Traversable) {
+                if ($this->recursionDepth < $this->maxRecursionDepth) {
+                   $this->recursionDepth++;
+                    foreach ($value as $v) {
+                        $data[$field][] = $this->serialize($v);
+                    }
+                    $this->recursionDepth--;
+                }
+            }
+            else if ($value instanceof \DateTime) {
+                $data[$field] = $value->format(\DateTime::ATOM);
+            }
+            else if (is_object($value)) {
+                $data[$field] = $this->serialize($value);
+            }
+            else {
+                $data[$field] = $value;
+            }
+        }
+
+        return $data;
+    }
+
+    /**
+     * Serialize a document to an array
+     *
+     * @param Document\Base $document
+     * @return array
+     */
+    public function toArray($document)
+    {
+        return $this->serialize($document);
+    }
+
+    /**
+     * Convert a document to a JSON object
+     *
+     * @param Document\Base $document
+     * @return string
+     */
+    public function toJson($document)
+    {
+        return json_encode($this->toArray($document));
+    }
+}
