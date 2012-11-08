f25b8850aeb5fea27933bf7aa130b7cbc2f16acd
Jérémy Huet <jeremy.huet@gmail.com>
2012-11-09 00:01:05 +0100

    New way of executing jasmine tests to allow usage of requirejs and dependencies.

--- public/test/jasmine/spec/example.js
+++ public/test/jasmine/spec/example.js
@@ -1,73 +1,89 @@
-describe("one tautology", function() {
-  it("is a tautology", function() {
-    expect(true).toBeTruthy();
-  });
-
-  describe("is awesome", function() {
-    it("is awesome", function() {
-      expect(1).toBe(1);
+describe("dependencies", function() {
+    describe("testing with jQuery", function() {
+        it("is jaqk", function() {
+            expect($('#ok').html()).toEqual('1');
+        });
     });
-  });
+
+    describe("testing with lodash", function() {
+        it("is mapness", function() {
+            expect(_.map([0, 1, 2], function(num) {
+                return ++num;
+            })).toEqual([1, 2, 3]);
+        });
+    });
+});
+
+describe("one tautology", function() {
+    it("is a tautology", function() {
+        expect(true).toBeTruthy();
+    });
+
+    describe("is awesome", function() {
+        it("is awesome", function() {
+            expect(1).toBe(1);
+        });
+    });
 });
 
 describe("simple tests", function() {
-  it("increments", function() {
-    var mike = 0;
+    it("increments", function() {
+        var mike = 0;
 
-    expect(mike++ === 0).toBeTruthy();
-    expect(mike === 1).toBeTruthy();
-  });
+        expect(mike++ === 0).toBeTruthy();
+        expect(mike === 1).toBeTruthy();
+    });
 
-  it("increments (improved)", function() {
-    var mike = 0;
+    it("increments (improved)", function() {
+        var mike = 0;
 
-    expect(mike++).toBe(0);
-    expect(mike).toBe(1);
-  });
+        expect(mike++).toBe(0);
+        expect(mike).toBe(1);
+    });
 });
 
 describe("setUp/tearDown", function() {
-  beforeEach(function() {
-    // console.log("Before");
-  });
-
-  afterEach(function() {
-    // console.log("After");
-  });
-
-  it("example", function() {
-    // console.log("During");
-  });
-
-  describe("setUp/tearDown", function() {
     beforeEach(function() {
-      // console.log("Before2");
+        // console.log("Before");
     });
 
     afterEach(function() {
-      // console.log("After2");
+        // console.log("After");
     });
 
     it("example", function() {
-      // console.log("During Nested");
+        // console.log("During");
     });
-  });
+
+    describe("setUp/tearDown", function() {
+        beforeEach(function() {
+            // console.log("Before2");
+        });
+
+        afterEach(function() {
+            // console.log("After2");
+        });
+
+        it("example", function() {
+            // console.log("During Nested");
+        });
+    });
 });
 
 describe("async", function() {
-  it("multiple async", function() {
-    var semaphore = 2;
+    it("multiple async", function() {
+        var semaphore = 2;
 
-    setTimeout(function() {
-      expect(true).toBeTruthy();
-      semaphore--;
-    }, 500);
+        setTimeout(function() {
+            expect(true).toBeTruthy();
+            semaphore--;
+        }, 500);
 
-    setTimeout(function() {
-      expect(true).toBeTruthy();
-      semaphore--;
-    }, 500);
+        setTimeout(function() {
+            expect(true).toBeTruthy();
+            semaphore--;
+        }, 500);
 
-    waitsFor(function() { return semaphore === 0 });
-  });
+        waitsFor(function() { return semaphore === 0 });
+    });
 });
