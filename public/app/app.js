33f2694f9d211df892fe4d8213d24ef0bb627f8a
Jérémy Huet <jeremy.huet@gmail.com>
2012-11-09 00:37:06 +0100

    Spaces !

--- public/app/app.js
+++ public/app/app.js
@@ -1,68 +1,68 @@
 define([
-  // Libraries.
-  "jquery",
-  "lodash",
-  "backbone",
+    // Libraries.
+    'jquery',
+    'lodash',
+    'backbone',
 
-  // Plugins.
-  "plugins/backbone.layoutmanager"
+    // Plugins.
+    'plugins/backbone.layoutmanager'
 ],
 
 function($, _, Backbone) {
 
-  // Provide a global location to place configuration settings and module
-  // creation.
-  var app = {
-    // The root path to run the application.
-    root: "/"
-  };
+    // Provide a global location to place configuration settings and module
+    // creation.
+    var app = {
+        // The root path to run the application.
+        root: '/'
+    };
 
-  // Localize or create a new JavaScript Template object.
-  var JST = window.JST = window.JST || {};
+    // Localize or create a new JavaScript Template object.
+    var JST = window.JST = window.JST || {};
 
-  // Configure LayoutManager with Backbone Boilerplate defaults.
-  Backbone.LayoutManager.configure({
-    // Allow LayoutManager to augment Backbone.View.prototype.
-    manage: true,
+    // Configure LayoutManager with Backbone Boilerplate defaults.
+    Backbone.LayoutManager.configure({
+        // Allow LayoutManager to augment Backbone.View.prototype.
+        manage: true,
 
-    prefix: "app/templates/",
+        prefix: 'app/templates/',
 
-    fetch: function(path) {
-      // Concatenate the file extension.
-      path = path + ".html";
+        fetch: function(path) {
+            // Concatenate the file extension.
+            path = path + '.html';
 
-      // If cached, use the compiled template.
-      if (JST[path]) {
-        return JST[path];
-      }
+            // If cached, use the compiled template.
+            if (JST[path]) {
+                return JST[path];
+            }
 
-      // Put fetch into `async-mode`.
-      var done = this.async();
+            // Put fetch into `async-mode`.
+            var done = this.async();
 
-      // Seek out the template asynchronously.
-      $.get(app.root + path, function(contents) {
-        done(JST[path] = _.template(contents));
-      });
-    }
-  });
+            // Seek out the template asynchronously.
+            $.get(app.root + path, function(contents) {
+                done(JST[path] = _.template(contents));
+            });
+        }
+    });
 
-  // Mix Backbone.Events, modules, and layout management into the app object.
-  return _.extend(app, {
-    // Create a custom object with a nested Views object.
-    module: function(additionalProps) {
-      return _.extend({ Views: {} }, additionalProps);
-    },
+    // Mix Backbone.Events, modules, and layout management into the app object.
+    return _.extend(app, {
+        // Create a custom object with a nested Views object.
+        module: function(additionalProps) {
+            return _.extend({ Views: {} }, additionalProps);
+        },
 
-    // Helper for using layouts.
-    useLayout: function(options) {
-      // Create a new Layout with options.
-      var layout = new Backbone.Layout(_.extend({
-        el: "body"
-      }, options));
+        // Helper for using layouts.
+        useLayout: function(options) {
+            // Create a new Layout with options.
+            var layout = new Backbone.Layout(_.extend({
+                el: 'body'
+            }, options));
 
-      // Cache the refererence.
-      return this.layout = layout;
-    }
-  }, Backbone.Events);
+            // Cache the refererence.
+            return this.layout = layout;
+        }
+    }, Backbone.Events);
 
 });
--- public/app/config.js
+++ public/app/config.js
@@ -1,31 +1,31 @@
 // Set the require.js configuration for your application.
 require.config({
-    urlArgs: "bust=" + (new Date()).getTime(), // @todo change bust with revision number or App version ?
+    urlArgs: 'bust=' + (new Date()).getTime(), // @todo change bust with revision number or App version ?
 
-  // Initialize the application with the main application file.
-  deps: ["main"],
+    // Initialize the application with the main application file.
+    deps: ['main'],
 
-  paths: {
-    // JavaScript folders.
-    libs: "../assets/js/libs",
-    plugins: "../assets/js/plugins",
-    vendor: "../assets/vendor",
+    paths: {
+        // JavaScript folders.
+        libs:       '../assets/js/libs',
+        plugins:    '../assets/js/plugins',
+        vendor:     '../assets/vendor',
 
-    // Libraries.
-    jquery: "../assets/js/libs/jquery",
-    lodash: "../assets/js/libs/lodash",
-    backbone: "../assets/js/libs/backbone"
-  },
-
-  shim: {
-    // Backbone library depends on lodash and Zepto (jQuery replacement).
-    backbone: {
-      deps: ["lodash", "jquery"],
-      exports: "Backbone"
+        // Libraries.
+        jquery:     '../assets/js/libs/jquery',
+        lodash:     '../assets/js/libs/lodash',
+        backbone:   '../assets/js/libs/backbone'
     },
 
-    // Backbone.LayoutManager depends on Backbone.
-    "plugins/backbone.layoutmanager": ["backbone"]
-  }
+    shim: {
+        // Backbone library depends on lodash and Zepto (jQuery replacement).
+        backbone: {
+            deps: ['lodash', 'jquery'],
+            exports: 'Backbone'
+        },
+
+        // Backbone.LayoutManager depends on Backbone.
+        'plugins/backbone.layoutmanager': ['backbone']
+    }
 
 });
