31b6ccb1602c29ce697b3191112a360f9119bef0
Jérémy Huet <jeremy.huet@gmail.com>
2012-11-17 15:14:35 +0100

    Changing path to auto-remove cached doctrine files.

--- bin/tests-jhuet.sh
+++ bin/tests-jhuet.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # don't have the rights some time to regenerate them as they can be created by www-data with only 644 rights
-rm -rf module/Dibber/src/Dibber/Document/Hydrator/*.php
-rm -rf module/Dibber/src/Dibber/Document/Proxy/*.php
+rm -rf data/cache/Dibber/Document/Hydrator/*.php
+rm -rf data/cache/Dibber/Document/Proxy/*.php
 
 if [ -z $1 ]; then
   dir='tests/Dibber/'
