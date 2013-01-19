64222385388b1968076a1fc9408d07924e601ca2
Jérémy Huet <jeremy.huet@gmail.com>
2013-01-19 02:11:26 +0100

    Implementing EventManagerAwareInterface with trait.

--- module/Dibber/src/Dibber/Service/Base.php
+++ module/Dibber/src/Dibber/Service/Base.php
@@ -2,14 +2,20 @@
 
 namespace Dibber\Service;
 
+use Dibber\EventManager\EventManagerAwareTrait;
+use Dibber\EventManager\TriggerEventTrait;
 use Dibber\Mapper\MapperAwareInterface;
 use Dibber\Mapper\MapperAwareTrait;
 use Zend\ServiceManager\ServiceManagerAwareInterface;
+use Zend\EventManager\EventManagerAwareInterface;
 
 abstract class Base implements
     MapperAwareInterface,
-    ServiceManagerAwareInterface
+    ServiceManagerAwareInterface/*,
+    EventManagerAwareInterface  // commented as it makes unit tests seg fault :/ */
 {
+    use EventManagerAwareTrait;
+    use TriggerEventTrait;
     use MapperAwareTrait;
     use ServiceManagerAwareTrait;
 }
